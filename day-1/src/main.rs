f1df9aab015ac6fd62990568090f46ec01b1aaf3
Raivo Salkovskis <raivo@salkovskis.com>
2022-12-01 13:52:33 +0200

    fix gitignore

---
+++ day-1/src/main.rs
@@ -0,0 +1,47 @@
+use std::{fs};
+
+fn read_input() -> Vec<String> {
+    let file_path = "./src/input.txt";
+
+    let contents = fs::read_to_string(file_path)
+        .expect("Should have been able to read the file");
+
+    return Vec::from_iter(contents.split("\n").map(String::from));
+}
+
+fn main() {
+    let list_of_calories = read_input();
+
+    let mut list_of_elf_calories = parse_and_sort_list(list_of_calories);
+
+    let most_calories = list_of_elf_calories.get(0).unwrap();
+    println!("most calories: {most_calories}");
+
+    let top_three = most_calories + list_of_elf_calories.get(1).unwrap() + list_of_elf_calories.get(2).unwrap();
+
+    println!("combined of top three: {top_three}");
+}
+
+fn parse_and_sort_list(list_of_calories: Vec<String>) -> Vec<i32> {
+
+    let mut list_of_elf_calories = Vec::<i32>::new();
+
+    let mut calories_total = 0;
+
+    for item in &list_of_calories {
+        if item.eq("") {
+            list_of_elf_calories.push(calories_total);
+
+            calories_total = 0;
+
+            continue;
+        }
+
+        calories_total += item.parse::<i32>().unwrap();
+    }
+
+    list_of_elf_calories.sort();
+    list_of_elf_calories.reverse();
+
+    return list_of_elf_calories;
+}
